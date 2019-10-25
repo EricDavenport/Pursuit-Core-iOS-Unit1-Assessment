22add0130cd27cf84a7d251903062cadfcb9f624
Eric Davenport <eric_dav@lilmanns-Pursuit-MacBook.local>
2019-10-25 15:23:31 -0400

    repaired score counter and reset

--- BlackJack/BlackJack/Game.swift
+++ BlackJack/BlackJack/Game.swift
@@ -46,10 +46,14 @@ class Game {
     }
     
 // MARK: Computed Properties
+//    var hasMoreCards: Bool {
+//
+//        return !deck.isEmpty
+//    }
     var hasMoreCards: Bool {
-        // check if deck is not empty - return is still comtains cards in deck
-        return !deck.isEmpty
-    }
+    // check if deck is not empty - return is still comtains cards in deck
+    return !user.cards.isEmpty
+       }
     
     var randomComputerScore: Int {
         let randomComputerScore = Int.random(in: 18...23)
@@ -59,67 +63,124 @@ class Game {
 // MARK: Methods
 // MARK: new Game Method
     func newGame() {
+        user.score = 0
         deck.removeAll()
         user.cards.removeAll()
-        user.score = 0
     }
+    
 // MARK: stop Hit method
-    func stopHits(playerScore: Int, computerScore: Int) -> String {
-        let computerScore = randomComputerScore
-        let playerScore = user.score
-        var winner = ""
-        if playerScore == 21 {
-            winner += player.playerName
-            print("\(winner) wins")
-        } else if computerScore == 21 {
-            winner = "Computer"
-            print("\(winner) wins!!!!")
-        } else if computerScore > 21 && playerScore < 21 {
-            print("House Bust!! \(player.playerName) Wins!!")
-        } 
-        return winner
-    }
+//    func stopHits(playerScore: Int, computerScore: Int) -> String {
+//        let computerScore = randomComputerScore
+//        let playerScore = user.score
+//        var winner = ""
+//        if playerScore == 21 {
+//            winner += player.playerName
+//            print("\(winner) wins")
+//        } else if computerScore == 21 {
+//            winner = "Computer"
+//            print("\(winner) wins!!!!")
+//        } else if computerScore > 21 && playerScore < 21 {
+//            print("House Bust!! \(player.playerName) Wins!!")
+//        }
+//        return winner
+//    }
+    
+    func stopHit(userScore: Int) {
+           let computerScore = randomComputerScore
+           let userScore = user.score
+           
+           if computerScore == 21 && userScore == 21 {
+               print("Draw!!")
+           } else if userScore == 21 {
+               print("\(user.playerName) wins!!!!")
+           } else if computerScore == 21 {
+               print("House Wins!!!")
+           } else if computerScore > 21 && userScore < 21 {
+               print("\(user.playerName) wins!!!!")
+           }
+       }
+    
+    
+    
+    
 // MARK: hit Me method
-    func hitMe(userInput: String, cards: [Card], playerScore: Int, computerScore: Int) {
-        var shuffledDeck = cards.shuffled()
-        let randomCard = shuffledDeck.popLast() ?? nil
+//    func hitMe(userInput: String, cards: [Card], playerScore: Int, computerScore: Int) {
+//        var shuffledDeck = cards.shuffled()
+//        let randomCard = shuffledDeck.popLast() ?? nil
+//
+//        switch userInput {
+//        case "hit":
+//            user.cards.append(randomCard!)
+//            gameStatus(playerCards: user.cards)
+//            print(randomCard)
+//        case "pass":
+//            gameStatus(playerCards: user.cards)
+//            stopHits(playerScore: user.score, computerScore: (Int.random(in: 18...23)))
+//            print("\(player.playerName): \(playerScore)\nComputer Score: \(computerScore)")
+//
+//        default:
+//            print("Invalid selection")
+//        }
+//        }
+    
+    func hitMe(userInput: String) -> Card? {
+        let shuffledDeck = deck.shuffled()
+        let randomCard = shuffledDeck.randomElement() ?? Card(suit: .heart, value: 2, isFaceCard: false, face: .none)
         
-        switch userInput {
-        case "hit":
-            user.cards.append(randomCard!)
-            print(randomCard)
-        case "pass":
-            stopHits(playerScore: playerScore, computerScore: computerScore)
-            print("")
-        default:
-            print("Invalid selection")
+        if userInput == "hit" {
+            user.cards.append(randomCard)
+            user.score += randomCard.value
+            print(shuffledDeck.count)
+        } else if userInput == "pass" {
+            stopHit(userScore: user.score)
         }
-        }
-
+        return randomCard
+        
+    }
+    
+    
 
 //    func computerVsPlayer(playerScore: Int, compScore: Int) -> String {
 //
 //    }
 // MARK: gameStatus method
+//    func gameStatus(playerCards: [Card]) {
+//        var playerScoreStatus: Int = 0
+//        for card in playerCards {
+//            playerScoreStatus += card.value
+//        }
+//            switch playerScoreStatus {
+//            case 21:
+//                print("Blackjack - YOU WIN!!!")
+//                game.newGame()
+//                break
+//            case 22...:
+//                print("BUST")
+//                game.newGame()
+//                break
+//            default:
+//                print()
+//            }
+//
+//    }
+
     func gameStatus(playerCards: [Card]) {
-        var playerScoreStatus: Int = 0
-        for card in playerCards {
-            playerScoreStatus += card.value
-        }
-            switch playerScoreStatus {
-            case 21:
-                print("Blackjack - YOU WIN!!!")
-                break
-            case 22...:
-                print("BUST")
-                break
-            default:
-                print()
-            }
+        var score: Int = 0
         
+            for card in playerCards {
+                score += card.value
+        
+                switch score {
+                case 21:
+                    print("BLACKJACK")
+                case 22...:
+                    print("BUST!!!!!")
+                default:
+                    print(" ")
+                
+            }
+        }
     }
-
-
 
 
 }
--- BlackJack/BlackJack/main.swift
+++ BlackJack/BlackJack/main.swift
@@ -12,31 +12,15 @@ import Foundation
 
 print("Please input your name.")
 var playerNameInput = readLine() ?? ""
-var user = Player(cards: [], playerName: playerNameInput)
 
+var user = Player(cards: [], playerName: playerNameInput)
 let game = Game(deck: Card.newDeck(aceValue: 1), player: user, hitPlayer: false)
-game.deck = Card.newDeck(aceValue: 1)
+
 
 var gameOver: Bool = false
 var continueGame: Bool = false
 
-//var currentGame: Bool = true
-// TODO: remove these lines after you have added the Suit and FaceCard enums as per the assessment README
-// print("There are \(Card.newDeck(aceValue: 1).count) in a deck of cards")
-// There are 52 in a deck of cards
-
-
-
-
-//var myDeck = Card.newDeck(aceValue: 1)
-
-
-//print(game.deck)
-
 repeat {
-    
-    
-
 
 repeat {
 
@@ -46,10 +30,9 @@ repeat {
     
   
 if passOrHit == "hit" {
-    game.hitMe(userInput: passOrHit ?? "", cards: game.deck, playerScore: user.score, computerScore: 0)
-    for each in user.cards {
-        user.score += each.value
-    }
+    game.hitMe(userInput: passOrHit ?? "")
+    
+            
     game.gameStatus(playerCards: user.cards)
     gameOver = true
     if user.score >= 21 {
@@ -58,13 +41,13 @@ if passOrHit == "hit" {
             continueGame = false
     }
 } else if passOrHit == "pass" {
-    game.stopHits(playerScore: user.score, computerScore: 0)
+    game.stopHit(userScore: user.score)
     game.gameStatus(playerCards: user.cards)
-    game.newGame()
     gameOver = false
     continueGame = false
 }
-        
+      
+
 print(game.deck.count)
 print()
 print(user.score, user.playerName)
@@ -75,15 +58,17 @@ print()
 } while user.score < 21
 
     print("Would you like to play again? Y/N")
-    var continuePlaying = readLine()?.uppercased()
+    let continuePlaying = readLine()?.uppercased()
     if continuePlaying == "Y" {
+        game.newGame()
         gameOver = true
     } else {
+        game.newGame()
         gameOver = false
     }
 
-   
+   game.newGame()
     
 } while gameOver
 
-game.newGame()
+
